460df7daf4acaba960097427e0f64aab5907c631
NajjwaAqilla <wawaqilla07@gmail.com>
2026-03-30 11:07:30 +0800

    Add files via upload

---
+++ 9.js
@@ -0,0 +1,19 @@
+// Array for image filenames
+var filenames = [
+    "G.jpg",
+    "A.jpg",
+    "E.jpg"
+];
+
+// Array for painting titles
+var titles = [
+    "Girl with a Pearl Earring",
+    "Artist Holding a Thistle",
+    "Portrait of Eleanor of Toledo"
+];
+
+// Array for quantities
+var quantities = [3, 1, 2];
+
+// Array for prices
+var prices = [80, 125, 75];
